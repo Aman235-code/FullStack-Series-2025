559070fc8ec4a17b80a4a012e11cdcf4d7683602
Aman Ahamed <amanahamed04gmail.com>
2025-02-04 21:57:42 +0530

    docker

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 
@@ -6,10 +6,17 @@ import Navbar from "./Navbar";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Home = () => {
   const [title, settitle] = useState("");
   const [desc, setdesc] = useState("");
+  const [todos, settodo] = useState([]);
 
   const addTodoHandler = async () => {
     try {
@@ -25,12 +32,31 @@ const Home = () => {
       );
 
       if (res.data.success) {
+        setdesc("");
+        settitle("");
+        settodo([...todos, res.data.todo]);
         toast.success(res.data.message);
       }
     } catch (error) {
       toast.error(error.response.data.message);
     }
   };
+
+  useEffect(() => {
+    const fetchTodo = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8000/api/v1/todo/getTodos"
+        );
+        if (res.data.success) {
+          settodo(res.data.todos);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTodo();
+  }, []);
   return (
     <div>
       <Navbar />
@@ -51,6 +77,21 @@ const Home = () => {
         placeholder="write a description"
         className="w-1/4 mt-2"
       />
+
+      <div className="grid grid-cols-5 gap-2">
+        {todos.map((todo) => (
+          <Card key={todo._id} className="bg-gray-800 text-white mt-5">
+            <CardHeader>
+              <CardTitle>
+                <h1>{todo.title}</h1>
+              </CardTitle>
+            </CardHeader>
+            <CardDescription>
+              <p>{todo.description}</p>
+            </CardDescription>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
